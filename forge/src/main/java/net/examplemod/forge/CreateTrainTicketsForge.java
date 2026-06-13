b5076db21886e95cfd9e6c022dfec2d193866c93
Lucas-alt395 <lucasbednar8@gmail.com>
2026-06-13 11:57:14 +0200

    wow

--- forge/src/main/java/net/examplemod/forge/CreateTrainTicketsForge.java
+++ forge/src/main/java/net/examplemod/forge/CreateTrainTicketsForge.java
@@ -6,8 +6,8 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(ExampleMod.MOD_ID)
-public class ExampleModForge {
+@Mod(CreateTrainTickets.MOD_ID)
+public class CreateTrainTicketsForge {
     public ExampleModForge() {
         // registrate must be given the mod event bus on forge before registration
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
