d33af01b22a3ec7e523ae778506de3caeec03d77
Lucas-alt395 <lucasbednar8@gmail.com>
2026-06-13 09:22:40 +0200

    Rename ExampleMod to CreateTrainTickets class

--- common/src/main/java/net/examplemod/CreateTrainTickets.java
+++ common/src/main/java/net/examplemod/CreateTrainTickets.java
@@ -5,7 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod {
+ public class CreateTrainTickets {
     public static final String MOD_ID = "createtraintickets";
     public static final String NAME = "Create: Train Tickets";
     public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
