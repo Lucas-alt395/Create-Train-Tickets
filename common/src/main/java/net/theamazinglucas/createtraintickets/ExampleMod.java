1222efc1d4a0d786b03a7c9bc665e074da60f966
Lucas-alt395 <lucasbednar8@gmail.com>
2026-06-14 09:46:35 +0200

    Add files via upload

---
+++ common/src/main/java/net/theamazinglucas/createtraintickets/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.theamazinglucas.createtraintickets;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "create_train_tickets";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
